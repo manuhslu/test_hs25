ab5c5cc912513760890300c1e63954b063e95a88
manuhslu <manuel.fluehler@stud.hslu.ch>
2025-10-06 21:21:34 +0200

    Save latest Streamlit setup and code

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 import pandas as pd
 import ifcopenshell as ifc
+import tkinter as tk
+import streamlit as st  
 print("hello world")
+
---
+++ xyz.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Hallo HSLU 👋")
+st.write("Wenn du das siehst, funktioniert Streamlit korrekt!")
